843ab72cf58159a216775fd68079e0c625116495
Angelina <angelina.kuptcova@gmail.com>
2022-05-20 18:21:56 +0300

    added formatting for currency

--- src/features/incomes/components/BudgetItem/BudgetItem.tsx
+++ src/features/incomes/components/BudgetItem/BudgetItem.tsx
@@ -5,6 +5,7 @@ import './budgetItem.scss'
 import { IBudgetItem } from '../../types/incomsTypes'
 import { TIME_FORMAT } from '../../../../CONSTANTS/TIME'
 import { OPTION_TYPES } from '../../../../CONSTANTS/CONSTANTS'
+import { convertToNumberWithCommas } from '../../../../utils/utils'
 
 export const BudgetItem: FC<IBudgetItem> = ({
     date, 
@@ -12,9 +13,12 @@ export const BudgetItem: FC<IBudgetItem> = ({
     type,
     category
 }) => {
+    const convertedSum = convertToNumberWithCommas(sum)
+    const formattedDate = dayjs(date).format(TIME_FORMAT.STANDARD)
+
     return <div className={`budgetItem ${type === OPTION_TYPES.INCOMES ? 'incomes' : 'expenses'}`}>
         <div className="budgetItem__category">{category}</div>
-        <div className="budgetItem__date">{dayjs(date).format(TIME_FORMAT.STANDARD)}</div>
-        <div className="budgetItem__sum">{sum}р</div>
+        <div className="budgetItem__date">{formattedDate}</div>
+        <div className="budgetItem__sum">{convertedSum}р</div>
     </div>
 }
